2e4109219bce1518429cb184d247694ab6e93cb7
Vince Buffalo <vsbuffalo@ucdavis.edu>
2012-04-30 16:27:22 -0700

    ggplot roc curve of results

--- new-testing/analysis.R
+++ new-testing/analysis.R
@@ -93,11 +93,15 @@ compareFastqTrim <- function(original, trimmed) {
 }
 
 
-calcStats <- function(x, y) {
-  stopifnot(length(x) == length(y))
-  n <- length(x)
-  cc <- sum(x == y)
-  data.frame(n.correct=cc, n.incorrect=n-cc)
+calcStats <- function(n.trimmed, n.contam) {
+  stopifnot(length(n.trimmed) == length(n.contam))
+  n <- length(n.trimmed)
+  false.positives <- sum(n.trimmed > 0 & n.contam == 0)
+  true.positives <- sum(n.trimmed == n.contam & n.contam > 0)
+  true.negatives <- sum(n.trimmed == n.contam & n.contam == 0)
+  false.negatives <- sum(n.trimmed == 0 & n.contam > 0)
+  data.frame(false.positives, true.positives,
+             false.negatives, true.negatives, total=n)
 }
 
 ## Now compare trimmed reads with original.
@@ -135,3 +139,21 @@ results <- with(trimmed.read.files,
 # results at the trimmer, contamination, and parameter level.
 d <- do.call(rbind, results)
 write.table(d, file="testing-results-table.txt", sep="\t", quote=FALSE, row.names=FALSE)
+
+addRates <- function(x) 
+# add FP and TP rates for generating ROC curves
+{
+  x$tpr <- with(x, true.positives/(true.positives + false.negatives))
+  x$fpr <- with(x, false.positives/(false.positives + true.negatives))
+  x
+}
+
+d <- addRates(d)
+
+## summarized d; take mean across replicates. We remove
+remove.cols <- which(colnames(d) %in% c("trimmer", "parameter", "rep", "contam.rate", "total"))
+ds <- aggregate(d[, -remove.cols], list(trimmer=d$trimmer, parameter=d$parameter), mean)
+p <- ggplot(ds) + geom_text(aes(x=fpr, y=tpr, color=trimmer, label=parameter), size=5)
+p <- p + scale_y_continuous("true positive rate")
+p <- p + scale_x_continuous("false positive rate")
+p
